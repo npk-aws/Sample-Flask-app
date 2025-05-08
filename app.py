2ffeaa785a660d449f95ba25cb38807a003e33c3
Karthik <karthiknp836@gmail.com>
2025-05-08 17:11:01 +0530

    Create app.py
    Flask app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+@app.route('/greet/<name>')
+def greet(name):
+    return jsonify(message=f"Hello, {name}!")
+
+if __name__ == '__main__':
+    app.run(debug=True)
